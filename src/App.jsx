c28708bbf8075d93eaa2aeaa3ffe37517754067d
Grishma Ashok Chichghare <chichgharegrishma1109@gmail.com>
2026-03-27 16:05:07 +0530

    update to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,27 @@
 import { useState}from "react";
 import "./App.css";
 
-function myFunc(value){
-    let abc = value;
-
-    function change(v1){
-        abc = v1;
-    }
-    
-    return [abc,change];
-}
-
-const [heading, setHeading] = myFunc("hello World");
-
 
 const MyApp = () => {
-    const [a,b] = useState("Hello World");
+    const [heading,setHeading] = useState("Hello World");
+    const [counter,setCounter] = useState(0);
 
     const onChangeHeading = () => {
-        b("Heading has been changed");
+        setHeading("Heading has been changed");
     }
 
     return (
         <div className="main-cont">
-            <h1>{a}</h1>
+            <h1>{heading}</h1>
             <br />
             <button onClick={onChangeHeading} className="btn btn-primary">change heading</button>
+            <br />
+            <h1 className="my-heading">{counter}</h1>
+            <br />
+            <div>
+                <button onClick={()=>{setCounter(counter-1)}} className="btn btn-danger mr-3">DEC</button>
+                <button onClick={()=>{setCounter(counter+1)}} className="btn btn-success">INC</button>
+            </div>
         </div>
     );
 }
