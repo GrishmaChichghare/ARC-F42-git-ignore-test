4177e29f3fde5380dceddb692a5ca60d363401d4
Grishma Ashok Chichghare <chichgharegrishma1109@gmail.com>
2026-04-02 15:29:11 +0530

    https://github.com/GrishmaChichghare/ARC-F42-git-ignore-tests-.git

--- viteapp/src/App.jsx
+++ viteapp/src/App.jsx
@@ -1,37 +1,10 @@
 import './App.css';
-import { useEffect, useState } from 'react';
 
-const MyApp = () => {
-
-    const [allValues, setValues] = useState({
-        heading : ""
-    });
-    
-    useEffect(() => {
-        const fetchJokes = async() => {
-            const api = "https://apis.ccbp.in/jokes/random";
-
-            try{
-                const response = await fetch(api);
-                const data = await response.json();
-
-                console.log(data);
-
-                setValues({...allValues, heading : data.value});
-            }
-            catch (error) {
-                console.log(error);
-            }
-        }
-        fetchJokes();
-    },[])
+const MyApp = () =>{
     return(
         <div className='main-cont'>
-            {/* <button onClick={fetchJokes} className='btn btn-primary'>Fetch Data</button> */}
-            <br /><br />
-            <h2>{allValues.heading}</h2>
+            <button className='btn btn-primary'>Fetch Data</button>
         </div>
     )
 }
-
-export default MyApp;                      
+export default MyApp;
