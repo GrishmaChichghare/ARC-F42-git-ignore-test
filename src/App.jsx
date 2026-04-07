8d46841b661e0de900750064a0fc5c2879fb6723
Grishma Ashok Chichghare <chichgharegrishma1109@gmail.com>
2026-04-07 15:15:35 +0530

    update to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
 import './App.css';
 
-const MyApp = () =>{
 
+const MyApp = () =>{
+    const [allValues,setValues] = useState({
+        heading;
+    });
+    const fetchJokes = async () => {
+        const api = "https://apis.ccbp.in/jokes/random";
+
+        try{
+            const response = await fetch(api);
+            const data = await response.json();
+            console.log(data);
+            setValues({...allValues,heading : data.value});
+        }
+        catch(error){
+            console.log(error);
+        }
+    }
     return(
         <div className='main-cont'>
-            <button className='btn btn-primary'>Fetch Data</button>
+            <button onClick={fetchJokes} className='btn btn-primary'>Fetch Data</button>
+            <br /><br />
+            <h2>{allValues.heading}</h2>
         </div>
     )
     
