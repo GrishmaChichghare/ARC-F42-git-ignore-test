3ecb00852500d270219b52fa694622249f83cba5
Grishma Ashok Chichghare <chichgharegrishma1109@gmail.com>
2026-04-14 10:46:59 +0530

    update to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import{useRef} from 'react';
 import './App.css';
 
+const myStyle = {
+    color : "blue",
+    fontSize : "60px"
+};
 
 const App = () => {
 
@@ -8,12 +12,12 @@ const App = () => {
     return(
         <div className='main-cont'>
 
-            <h1 style={{color: "red"}}>0</h1>
+            <h1 style={myStyle}>0</h1>
             <br />
 
             <div>
-                <button onClick={() => {}} className='btn btn-primary mr-4'>INC</button>
-                <button onClick={() => {}} className='btn btn-primary'>DEC</button>
+                <button onClick={() => {}} className='btn btn-danger mr-4'>INC</button>
+                <button onClick={() => {}} className='btn btn-success'>DEC</button>
             </div>
         </div>
         
