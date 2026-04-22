eaff3cc0a370b5c10652b2026ef25ff0ffbe5b1c
Grishma Ashok Chichghare <chichgharegrishma1109@gmail.com>
2026-04-22 16:07:50 +0530

    update to app.jsx

---
+++ src/components/job/index.jsx
@@ -0,0 +1,6 @@
+import './index.css';
+
+const Job = () => {
+    return <h1 style={{ color: 'blue' }}>Job Component</h1>
+}
+export default Job;
---
+++ src/components/login/index.jsx
@@ -0,0 +1,6 @@
+import './index.css';
+
+const Login = () => {
+    return <h1 style={{ color: 'green' }}>Login Component</h1>
+}
+export default Login;
