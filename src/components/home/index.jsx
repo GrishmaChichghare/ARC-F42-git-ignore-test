df324c4559f1cc992b971aebad5237bf9bdccd10
Grishma Ashok Chichghare <chichgharegrishma1109@gmail.com>
2026-04-23 23:27:10 +0530

    update to jobapp/index.jsx

--- src/components/home/index.jsx
+++
@@ -1,7 +0,0 @@
-import './index.css';
-
-const Home = () => {
-
-    return <h1 style={{ color: 'red' }}> Home Component</h1>
-}
-export default Home;
--- src/components/job/index.jsx
+++
@@ -1,6 +0,0 @@
-import './index.css';
-
-const Jobs = () => {
-    return <h1 style={{ color: 'blue' }}>Jobs Component</h1>
-}
-export default Jobs;
