f2a3f75b9a87f64337d468d7dee4eacb5d1bb96f
Grishma Ashok Chichghare <chichgharegrishma1109@gmail.com>
2026-04-23 13:52:16 +0530

    update to jobapp/app.jsx

--- jobapp/src/App.jsx
+++ jobapp/src/App.jsx
@@ -1,8 +1,20 @@
+import Home from './components/home';
+import Jobs from './components/job';
+import LogIn from './components/login';
+import { Routes, Route } from 'react-router-dom';
+import NotFound from './components/notFound';
+
 const App = () => {
   return (
     
-      <h1>App component</h1>
+    <Routes>
+      
+      <Route path='/' element={<Home />}></Route>
+      <Route path='/jobs' element={<Jobs />}></Route>
+      <Route path='/login' element={<LogIn />}></Route>
+      <Route path='/*' element={<NotFound />}></Route>
     
+    </Routes>
   );
 }
 export default App;
