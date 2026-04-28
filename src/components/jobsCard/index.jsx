1447224931d075886f169e65eef55d870e0dba04
Grishma Ashok Chichghare <chichgharegrishma1109@gmail.com>
2026-04-28 22:18:04 +0530

    update to jobapp/index.jsx

--- src/components/jobsCard/index.jsx
+++ src/components/jobsCard/index.jsx
@@ -1,5 +1,16 @@
 import './index.css';
 
+// {
+//             "id": "bb95e51b-b1b2-4d97-bee4-1d5ec2b96751",
+//             "title": "Devops Engineer",
+//             "rating": 4,
+//             "company_logo_url": "https://assets.ccbp.in/frontend/react-js/jobby-app/netflix-img.png",
+//             "location": "Delhi",
+//             "job_description": "We are looking for a DevOps Engineer with a minimum of 5 years of industry experience, preferably working in the financial IT community. The position in the team is focused on delivering exceptional services to both BU and Dev partners to minimize/avoid any production outages. The role will focus on production support.",
+//             "employment_type": "Internship",
+//             "package_per_annum": "10 LPA"
+//         }
+
 const JobsCard = ()=> <h1>Jobs Card Component</h1>
 
 export default JobsCard;
